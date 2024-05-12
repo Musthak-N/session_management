9a8b7ded7c6d41cd72e58bc05a82e752d9321363
Musthak-N <musthak.dev786@gmail.com>
2024-05-12 20:44:11 +0530

    yyyy

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 6661;
 const redisClient = redis.createClient({
  enable_offline_queue: false,
  //  password: "SBH_PROJECT",
- URL: "redis://red-cp0b81a1hbls73e7b1cg:6379",
+ URL: "rediss://red-cp0b81a1hbls73e7b1cg:6lqeV7LfDkjdDgcdbK2vV1zzfsChlWDs@oregon-redis.render.com",
  //  host: "redis://red-cp0b81a1hbls73e7b1cg:6379",
  //  port: 6379,
 });
